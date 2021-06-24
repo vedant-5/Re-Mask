7430f91bfdc7bc5d592aa4bd5ed35e921d1c089d
vedant-5 <65765943+vedant-5@users.noreply.github.com>
2021-06-24 22:20:18 +0530

    Delete dutils.py

--- dutils.py
+++
@@ -1,9 +0,0 @@
-mongodb_url = 'mongodb://127.0.0.1:27017/'
-mongodb_name = 'Remask'
-from pymongo import MongoClient
-
-def mongodb_connection(collection: str):
-    client = MongoClient(mongodb_url)
-    database = client[mongodb_name]
-    collection = database[collection]
-    return collection
